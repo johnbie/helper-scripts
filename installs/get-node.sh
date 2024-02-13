b6e878db2278b97d7e73745fa0039b8169d5dfc3
John Lee <johnqwerty.lee1@gmail.com>
2024-02-13 15:44:30 -0800

    Improved get-node helper script

--- installs/get-node.sh
+++ installs/get-node.sh
@@ -1,3 +1,15 @@
-# apt install nodejs -y # check
-apt install nvm -y
+#!/usr/bin/env bash
+
+# install npm
 apt install npm -y
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# get latest node version via nvm
+nvm install node
+
