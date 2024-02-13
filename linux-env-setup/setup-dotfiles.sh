2b837f6889841a45e28b119ccf02eb42282a63f5
John Lee <johnqwerty.lee1@gmail.com>
2024-02-13 14:49:10 -0800

    Added helper for setting up dotfiles

---
+++ linux-env-setup/setup-dotfiles.sh
@@ -0,0 +1,31 @@
+backup_file() {
+    if [ -f "$1" ]&& [ ! -h "$1" ]; then
+        mv "$1" "${1}_back"
+    fi
+}
+
+# install stow
+apt install stow
+
+# create new variable
+dotfiles_dir="$HOME/dotfiles"
+
+# Check if dotfiles directory exists
+if [ ! -d "$dotfiles_dir" ]; then
+    echo "Error: dotfiles directory '$dotfiles_dir' not found."
+    exit 1
+fi
+
+# Loop through all files in the dotfiles directory
+for file in "$dotfiles_dir"/* "$dotfiles_dir"/.*; do
+     # Check if the item is a file and not a symbolic link
+    if [ -f "$file" ]; then
+        # Get the filename without path
+        filename=$(basename "$file")
+        backup_file "$HOME/${filename}"
+    fi
+done
+
+# stow it
+stow -d "$dotfiles_dir" .
+
