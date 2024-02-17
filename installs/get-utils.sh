2e011cfcdc346fb6708f82b0e6512f383b23be74
John Lee <johnqwerty.lee1@gmail.com>
2024-02-16 18:49:28 -0800

    Sudo

--- installs/get-utils.sh
+++ installs/get-utils.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apt install tree -y
-apt install youtube-dl -y
-apt install jq -y
-apt install exa -y
+sudo apt install tree -y
+sudo apt install youtube-dl -y
+sudo apt install jq -y
+sudo apt install exa -y
