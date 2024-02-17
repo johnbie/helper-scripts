fb03f1612569272fff89d42e504419da8a9072d4
Jangwon Lee <johnqwerty.lee1@gmail.com>
2024-02-16 18:32:06 -0800

    Removed gpg key creation

--- installs/create-ssh-keygen.sh
+++ installs/create-ssh-keygen.sh
@@ -1,5 +1,2 @@
 ssh-keygen -t rsa -b 4096 -C "johnqwerty.lee1@gmail.com"
 chmod 0600 ~/.ssh/id_rsa*
-
-gpg --generate-key
-# pass init XXXXXXXXXXXXXX # the key you got from gpg that's around 14 characters long
