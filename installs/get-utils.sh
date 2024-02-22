3724c2f5880a58e6afab83f110fa0d44dc11b4c8
John Lee <johnqwerty.lee1@gmail.com>
2024-02-21 19:40:28 -0800

    Added link for get-utils

--- installs/get-utils.sh
+++ installs/get-utils.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+# some of the utils are from https://github.com/andrew8088/dotfiles/blob/main/install/install-deps-linux.sh
 sudo apt install tree -y
 sudo apt install youtube-dl -y
 sudo apt install jq -y
