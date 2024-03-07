a7d01cec3075eb40439c4b7556393ffec761c366
John Lee <johnqwerty.lee1@gmail.com>
2024-03-07 07:39:07 -0800

    Updated thefuck installer

--- installs/add-thefuck.sh
+++ installs/add-thefuck.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 # https://github.com/nvbn/thefuck
 
-sudo apt update
-sudo apt install python3-dev python3-pip python3-setuptools -y
-pip3 install thefuck --user
+# just brew it
+brew install thefuck
+
+#sudo apt update
+#sudo apt install python3-dev python3-pip python3-setuptools -y
+#pip3 install thefuck --user
 
 source ~/.profile # source profile to add `~/.local` to path
 fuck # auto-add alias
