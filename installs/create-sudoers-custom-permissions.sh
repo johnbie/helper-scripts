1aaa3f93b9263f7d9d0efbcb27100b44be9c1dd1
John Lee <johnqwerty.lee1@gmail.com>
2024-05-08 16:27:57 -0700

    Updated command

--- installs/create-sudoers-custom-permissions.sh
+++ installs/create-sudoers-custom-permissions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'A command used to allow users to run a command without writing `sudo` every time'
-echo 'Add permissions for users by passing username and command (i.e. `username ALL=(ALL) /usr/sbin/grub-reboot`)'
+echo 'Add permissions for users by passing username and command (i.e. `admin ALL=(ALL) /usr/sbin/grub-reboot`)'
 echo "Press Enter to continue..."
 read
 
